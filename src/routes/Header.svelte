2b845c23028d8602bc8aa291001c1d66748c14ae
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-01-21 16:13:11 +0800

    feat: add svelte-kit

---
+++ src/routes/Header.svelte
@@ -0,0 +1,110 @@
+<script>
+  import { page } from '$app/stores'
+  import Icon from '@iconify/svelte'
+  import kun from '$lib/images/favicon.webp'
+</script>
+
+<header>
+  <div class="corner">
+    <a href="https://github.com/KUN1007/kungalgame-stickers">
+      <img src={kun} alt="KUN Visual Novel | Stickers" />
+    </a>
+  </div>
+
+  <nav>
+    <ul>
+      <li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+        <a href="/">Home</a>
+      </li>
+      <li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+        <a href="/about">About</a>
+      </li>
+    </ul>
+  </nav>
+
+  <div class="corner">
+    <a href="https://github.com/sveltejs/kit">
+      <Icon icon="line-md:github-loop" />
+    </a>
+  </div>
+</header>
+
+<style lang="scss">
+  header {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .corner {
+    width: 3em;
+    height: 3em;
+  }
+
+  .corner a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+  }
+
+  .corner img {
+    width: 2em;
+    height: 2em;
+    object-fit: contain;
+  }
+
+  nav {
+    display: flex;
+    justify-content: center;
+    --background: rgba(255, 255, 255, 0.7);
+  }
+
+  ul {
+    position: relative;
+    padding: 0;
+    margin: 0;
+    height: 3em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    list-style: none;
+    background: var(--background);
+    background-size: contain;
+  }
+
+  li {
+    position: relative;
+    height: 100%;
+  }
+
+  li[aria-current='page']::before {
+    --size: 6px;
+    content: '';
+    width: 0;
+    height: 0;
+    position: absolute;
+    top: 0;
+    left: calc(50% - var(--size));
+    border: var(--size) solid transparent;
+    border-top: var(--size) solid var(--color-theme-1);
+  }
+
+  nav a {
+    display: flex;
+    height: 100%;
+    align-items: center;
+    padding: 0 0.5rem;
+    color: var(--color-text);
+    font-weight: 700;
+    font-size: 0.8rem;
+    text-transform: uppercase;
+    letter-spacing: 0.1em;
+    text-decoration: none;
+    transition: color 0.2s linear;
+  }
+
+  a:hover {
+    color: var(--color-theme-1);
+  }
+</style>
